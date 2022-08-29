4d4048c1ec3a9acd38d76436c3466e54b6e2acc3
cclkz <1964450091@qq.com>
2022-08-29 20:50:06 +0800

    feat:自定义布局

---
+++ layouts/blog.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <p>blog layout</p>
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'BlogPage',
+})
+</script>
