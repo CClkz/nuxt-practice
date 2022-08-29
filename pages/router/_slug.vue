6c408d3faa9961883cd383b58d9027532e2d439e
cclkz <1964450091@qq.com>
2022-08-29 20:23:30 +0800

    feat:动态嵌套路由

---
+++ pages/router/_slug.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><p>_slug</p><p><nuxt-child></nuxt-child></p></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'SlugPage',
+})
+</script>
---
+++ pages/router/_slug/_slug1.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><p>_slug1</p></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Slug1Page',
+})
+</script>
