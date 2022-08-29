4e62ca5653bed6899cc610432d44e7c368aa7336
cclkz <1964450091@qq.com>
2022-08-29 21:06:45 +0800

    feat:asyncData异步数据

---
+++ pages/axios/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>axios</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from '@nuxtjs/axios'
+
+export default Vue.extend({
+  name: 'AxiosPage',
+//   promise
+  //   asyncData({ params, $axios }) {
+  //     return $axios.get(`https://my-api/posts/${params.id}`).then((res: any) => {
+  //       return { title: res.data.title }
+  //     })
+  //   },
+//   async await
+  async asyncData({ params, $axios }) {
+    const res = await $axios.get(`https://my-api/posts/${params.id}`)
+    return { title: res.data.title }
+  },
+})
+</script>
